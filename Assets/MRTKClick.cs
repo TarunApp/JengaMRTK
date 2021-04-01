861ad323ad3378ae9232c80b4d201309c7cb6f39
unknown <tappannagari@gmail.com>
2021-04-01 15:18:14 -0700

    JengaMRTK

---
+++ Assets/MRTKClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.UI;
+
+
+public class MRTKClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+	// Interactable interact = gameObject.AddComponent<Interactable>();
+
+
+
+    void Start()
+    {
+    	Debug.Log("test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     	// Debug.Log("test");   
+     	Debug.Log("test");
+    }
+
+}
